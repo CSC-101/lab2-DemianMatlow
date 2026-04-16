d663cfd94fc2731c202329f5e3eb5619c4c8df40
DemianMatlow <dmatlow@calpoly.edu>
2026-04-16 14:24:06 -0700

    Completed task 3

--- main.py
+++ main.py
@@ -9,3 +9,30 @@ def welcome_message(name:str) -> str:
 
 message = welcome_message("dmatlow@calpoly.edu")
 print(message)
+
+
+def smallest(n: float, m: float) -> float:
+   if n < m:
+      return n  # It is not evaluated for either
+   else:
+      return m
+
+
+first = smallest(3, 2)  # First=2
+second = smallest(2, 2)  # Second=2. Not reasonable because neither are smaller, n and m are equal.
+print(first,second)
+
+
+def function2(a: int, b: int, c: int) -> int:
+   if a > b and a > c:
+      return a - b  # When a>b and a>c
+   elif b > c:
+      return b + c  # When a<=b or a<=c, and b>c
+   else:
+      return 2 * c  # When a<=b or a<=c, and b<=c
+
+
+answer1 = function2(3, 2, 1)  # answer1=1
+answer2 = function2(2, 3, 1)  # answer2=4
+answer3 = function2(2, 1, 3)  # answer3=6
+print(answer1,answer2,answer3)
