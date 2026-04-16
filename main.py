03514deb78b0ef45dd537e1a0ded72eb87389c40
DemianMatlow <dmatlow@calpoly.edu>
2026-04-16 14:26:36 -0700

    completed task 3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Create a welcome message.
 # Input: a name as a string
 # Result: a string
-def welcome_message(name:str) -> str:
+def welcome_message(name: str) -> str:
    message = "Hello, " + name + "."
 
    return message
